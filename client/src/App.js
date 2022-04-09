f14739a75e002d801648f2e4d22158edaa5977e8
rudovik <giguni@ukr.net>
2022-04-09 22:56:35 +0300

    my change

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <div className='App'>
         <header className='App-header'>
+          <h1>Fib Calculator</h1>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
